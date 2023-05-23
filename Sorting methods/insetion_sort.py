8eb21d7d3513fca0a7005c3732a7a957ea8f4605
Ludovico Passari <122237053+ludovicopassari@users.noreply.github.com>
2023-05-23 21:31:49 +0200

    Update insetion_sort.py

--- Sorting methods/insetion_sort.py
+++ Sorting methods/insetion_sort.py
@@ -1,35 +1,40 @@
-def insertion_sort(data):
+"""
+ALGORITMO INSERTION-SORT
+"""
+
+def insertion_sort(array):
     """
     Ordinamento di un'array implementato con l'algoritmo Insertion-Sort.
 
     Costo computazionale : O(n) nel caso migliore, un 0(n^2) nel caso peggiore
 
     Description: Ad ogni iterazione l'array è ordinato da 0 a j-1 e disordinato da j a n.
+
     """
+
     # dimensione dell'array
-    n = len(data)
+    n = len(array)
 
     for j in range(1, n):
         # elemento corrente
-        key = data[j]
-
+        key = array[j]
         # indice dell'elemento a sinistra di key
         i = j - 1
-        # finchè non arrivi all'indice 0 e finchè l'elemento in i è maggiore di elem
-        while i >= 0 and data[i] > key:
+        # finchè non arriva all'indice 0 e finchè l'elemento in i è maggiore di key
+        while i >= 0 and array[i] > key:
             # trasla a destra l'elemento in i
-            data[i + 1] = data[i]
+            array[i + 1] = array[i]
             i -= 1
         # aggiungi l'ultimo elemento
-        data[i + 1] = key
+        array[i + 1] = key
 
-    return data
+    return array
 
 
 def main():
-    data = [12, 5, 3, 4]
-    print("Unsorted Data -->", data)
-    print("Sorted Data -->", insertion_sort(data))
+    unsorted_array = [12, 5, 3, 4]
+    print("Unsorted Data -->", unsorted_array)
+    print("Sorted Data -->", insertion_sort(unsorted_array))
 
 
 if __name__ == "__main__":
