71df6fc2533ba5cd403ef080bd1e092a9c1a493c
Ludovico Passari <122237053+ludovicopassari@users.noreply.github.com>
2023-05-23 21:38:31 +0200

    Update merge_sort.py

--- Sorting methods/merge_sort.py
+++ Sorting methods/merge_sort.py
@@ -17,46 +17,64 @@ def merge(l_array, r_array, data):
     left_array_size = len(l_array)
     # dimensione del sottoarray di destra
     right_array_size = len(r_array)
-
+    
+    #finchè non termina uno dei due sotto-array
     while i < left_array_size and j < right_array_size:
+        #se l'elemento del sottoarray di sinistra è più grande di quello del sottoarray di destra.
         if l_array[i] > r_array[j]:
+            #aggiungi all'interno dell'array di output l'elemnto j-esimo del sotto-array di destra.
             data[k] = r_array[j]
             j += 1
 
         else:
             data[k] = l_array[i]
             i += 1
+        #incremente l'indice dell'array di output
         k += 1
 
-    # sposta gli ultimi elementi dall'array di sinistra o da quello di destra
+    # se l'indice 'i' non ha ancora spazzolato tutto il sotto-array di sinstra 
     if i < left_array_size:
+        #completa l'inserimento degli elementi nell'array di output
         while i < left_array_size:
             data[k] = l_array[i]
             i += 1
             k += 1
-
+    # se l'indice 'j' non ha ancora spazzolato tutto il sotto-array di destra 
     if j < right_array_size:
+        #completa l'inserimento degli elementi nell'array di output
         while j < right_array_size:
             data[k] = r_array[j]
             j += 1
             k += 1
 
-
 def merge_sort(data):
+    """
+    Implementazione dell'algoritmo Merge-Sort.
+    
+    Args: La funzione prende in input un array disordinato.
+    
+    Rerurn: Ritorna un array ordinato.
+    
+    """
     n = len(data)
-
+    #se l'array passato è composto almeno da due elementi
     if n > 1:
+        
+        #calcola il punto mediano dell'array
         mid = n // 2
         # effettua uno slice dall'indice 0 fino alla metà
         left_array = data[:mid]
         # effettua uno slice dall'indice mediano fino alla fine
         right_array = data[mid:]
-
+        
+        #richiama ricorsivamente la funzione
         merge_sort(left_array)
         merge_sort(right_array)
+        
         # fa un merge dei due array già ordinati
         merge(left_array, right_array, data)
-        return data
+        
+    return data
 
 
 def main():
