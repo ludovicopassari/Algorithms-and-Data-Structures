cfb4d9cb274dbea1d7ed3805e5c1a914f275811b
Ludovico Passari <122237053+ludovicopassari@users.noreply.github.com>
2023-05-23 23:19:22 +0200

    Update graph_traversal.py

--- Graph/Traversal/graph_traversal.py
+++ Graph/Traversal/graph_traversal.py
@@ -1,103 +1,121 @@
-def dfs(graph, current_vertex, visited=[]):
+"""  
+ATTRAVERSAMENTI DI UN GRAFO
+
+"""
+def dfs(graph, vertex, visited=[]):
     """
-    Implementazione ricorsiva dell'atteaversamento in profondità di un grafo:
+    La funzione implementa un attraversamento in profondita di un grafo ricorsivamente.
 
-    Args: LA funzione prende in input un grafo, il vertice da cui iniziare l'attraversamento e una list che conterrà tutti i nodi visitati.
+    Args: La funzione prende in input un grafo, un vertice da cui partire, e una lista che tiene traccia dei nodi visitati.
 
-    Return: Ritorna la lista dei nodi visitati.
+    Return: Ritorna la lista dei nodi visitai
 
-    Description: La funzione considera un nodo di partenza e lo visita poi se il nodo ha dei vicini e questi vicini non sono stati visitati , richiama la funzine dfs() passando ogni volta come paramentro il nodo vicino.
     """
-    # se il nodo corrente non è stato visitato
-    if current_vertex not in visited:
-        # visita il nodo
-        visited.append(current_vertex)
-        # per ogni vicino del nodo corrente
-        for neighbours in graph[current_vertex]:
-            # se il vicino non è stato visitato, visitalo
+    # se il nodo non è stato visitato
+    if vertex not in visited:
+        # visita il nodo corrente
+        visited.append(vertex)
+        # per ogni vicino del vertice
+        for neighbours in graph[vertex]:
+            # se il vicino non è stato visitato
             if neighbours not in visited:
+                # richiama la funzione ricorsivamente con il vicino passaro come argomento
                 dfs(graph, neighbours, visited)
+
     return visited
 
 
-def dfs_iterative(graph, u):
+def dfs_iterative(graph, start_vertex):
     """
-    La funzione implementa in maniera iterativa l'attraversamento in profondità di un grafo.
+    Implementazione iterativa di un attraversamento in profindità di un grafo .
+
+    Args: La funzione prende in input un grafo e un vertice da cui iniziare ad attaraversare.
+
+    Return: Ritorna la lista dei nodi visitati
 
     """
     visited = []
-    queue = []
-
-    queue.append(u)
+    queue = [start_vertex]
 
     while queue:
-        current_vertex = queue.pop()
+        curr = queue.pop()
 
-        if current_vertex not in visited:
-            visited.append(current_vertex)
-            for neighbours in graph[current_vertex]:
-                queue.append(neighbours)
-
+        # se il nodo corrente non è stato visitato
+        if curr not in visited:
+            # visita il nodo corrente
+            visited.append(curr)
+            # per ogni vicino
+            for neighbour in graph[curr]:
+                # se il vicino non è stato visitato
+                if neighbour not in visited:
+                    # mettilo in attesa per essere visitato
+                    queue.append(neighbour)
     return visited
 
 
-def dfs_color(graph, u):
-    exploration = {vertex: "white" for vertex in graph}
-
-    for vertex in graph:
-        if exploration[vertex] == "white":
-            visit_vertex(graph, vertex, exploration)
-
-    print(exploration)
-
-
-def visit_vertex(graph, vertex, exploration):
-    exploration[vertex] = "gray"
-
-    for neighbours in graph[vertex]:
-        if exploration[neighbours] == "white":
-            visit_vertex(graph, neighbours, exploration)
-    exploration[vertex] = "black"
-
-
 def bfs(graph, start_vertex):
-    """
-    Questa funzione implementa una visita in ampiezza di un grafo.
-
-    Args: Prende in input un grafo e un vertice da cui iniziare le visite.
-
-    Color code : {'white':not visited} {'gray': visit in process} {'black': visited}
-    """
-    visit = []
-    # crea una mappa delle visite in base ad un codice colore
+    # explortion_map ={vertex:'white' for vertex in graph}
     exploration_map = {vertex: "white" for vertex in graph}
+
+    queue = [start_vertex]
     exploration_map[start_vertex] = "gray"
 
-    stack = []
-    stack.append(start_vertex)
+    while queue:
+        curr = queue.pop()
 
-    while stack:
-        curr = stack.pop()
-
-        # per ogni vicino del nodo corrente
         for neighbour in graph[curr]:
-            # se il vicino è mappato come non visitato
             if exploration_map[neighbour] == "white":
-                # mette in stato di visita il vicino
-                exploration_map[curr] = "gray"
-                stack.append(neighbour)
+                exploration_map[neighbour] = "gray"
+                queue.append(neighbour)
 
-        # mappa il nodo corrente come visitato
         exploration_map[curr] = "black"
+    return exploration_map
 
-        # da valutare.......
-        if curr not in visit:
-            visit.append(curr)
 
-    return exploration_map, visit
+def dfs_color(graph, start_vertex):
+    """
+    Implmentazione di un attraversamento utilizzando un codice colori per identificare lo stato di visita di un vertice.
+
+    'white' -> not visited
+    'gray' -> in visit
+    'black' -> visited
+
+    Args: La funzione prende in input un grafo, e un vertice da cui inziare ad attraversare il grafo.
+
+    Return: Ritorna il dizionario con lo stato delle visite.
+
+    """
+    exploration_map = {vertex: "white" for vertex in graph}
+
+    for vertex in graph:
+        # se il vertice non è ancora stato visitato
+        if exploration_map[vertex] == "white":
+            visit_vertex(graph, exploration_map, vertex)
+    return exploration_map
+
+
+def visit_vertex(graph, exploration_map, vertex):
+    """
+    Funzione di appoggio per la visita in profondità che visita in profondità un vertice e tutti i suoi vicini.
+
+    Args: La funzione prende in input un grafo, un dizionario che contiele lo stato di esplorazione del grafo,e un vertice da visitare.
+
+    Return: None
+    """
+    # mette in stato di visita il vertice
+    exploration_map[vertex] = "gray"
+    # per ogni vicino del vertice
+    for neighbour in graph[vertex]:
+        # se il vertice non è stato visitato
+        if exploration_map[neighbour] == "white":
+            # visita in profondità il vertice
+            visit_vertex(graph, exploration_map, neighbour)
+
+    exploration_map[vertex] = "black"
 
 
 def main():
+    # grafo non orientato, pesato
     graph = {
         "ME": {"CT": 93.6, "PA": 225.8},
         "PA": {"CT": 209.6, "ME": 225.8, "TP": 108.5},
@@ -107,13 +125,11 @@ def main():
         "CL": {"SR": 159.6},
     }
 
-    print("DFS:", dfs(graph, "ME"))
-    print()
-    print("DFS iterative :", dfs_iterative(graph, "ME"))
-    print()
-    # print(dfs_color(graph, "ME"))
-    print("BFS:", bfs(graph, "ME"))
-    print()
+    print("DFS visit:", dfs(graph, "ME"))
+    print("DFS iterative visit:", dfs_iterative(graph, "ME"))
+    print("DFS COLOR visit:", dfs_color(graph, "ME"))
+    print("BFS visit :", bfs(graph, "ME"))
+
     print("Graph:", graph)
 
 
