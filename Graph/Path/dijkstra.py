1918fa05d695cf593621928b42579141dbd8ccae
Ludovico Passari <122237053+ludovicopassari@users.noreply.github.com>
2023-05-24 10:56:47 +0200

    Update dijkstra.py

--- Graph/Path/dijkstra.py
+++ Graph/Path/dijkstra.py
@@ -1,72 +1,72 @@
 """  
-Implementazione dell'algoritmo di Dijkstra per trovare il cammino minimo da un nodo A verso tutti gli atri nodi
+ALGORITMO DI DIJKSTRA
 """
+
+def min_pop(pq):
+    """
+    Effettua un pop dell'elemento minimo.
+
+    Args: La funzione prende in input una coda di priorità in cui ogni elemeneto è una tupla (w,vertex).
+
+    Return: Ritorna una tupla con l'elemento minimo.
+
+    """
+    # considera inizialmente come minimo la prima tupla di valori
+    min_w, min_v = pq[0]
+
+    # per ogni tupla nella coda
+    for item in pq:
+        # spacchetta i valori della tupla
+        curr_w, curr_v = item
+        # se il valore di peso corrente è minore di quello fin ora trovato
+        if curr_w < min_w:
+            # aggiorna il valore del peso
+            min_w = curr_w
+            # aggiorna il nome del vertice corrispondente
+            min_v = curr_v
+    # rimuove dalla coda la tupla trovata
+    pq.remove((min_w, min_v))
+
+    return min_w, min_v
 
 
 def dijkstra(graph, start):
     """
-    Algoritmo che trova i cammini minimi da un vertice sorgente a tutti gli altri
+    Implementazione algoritmo Dijkstra.
 
-    Args: La funzione prende in input un grafo pesato e il nome di un vertice da cui partire
+    Args: La funzione prende in input un grafo e un vertice di start
 
-    Return : Ritorna un dizionario con tutte le distanze dal nodo di start a tutti i inodi
-
-
-    1) Viene creato un dizioanrio con tutte le distanze dal vertice di start verso tutti gli altri vertici(inizialmente tutte le distanze sono settate ad 'inf' tranne quella del nodo di start)
-
-    2) Viene creata una coda in cui inizialmente viene incodato il primo vertice e la distanza 0
-
-    3)viene utilizzata una funzione di supporto che fa un pop dell'elemento con la distanza minima
-
-    4) Per ogni vicino del nodo corrente viene calcolata la distanza tra il nodo di partenza e quel nodo vicino e se è minore del valore memorizzato nel dizionario delle distanze, quel valore nel dizionario viene aggiornato e vien e viene incodata la tupla (w_calcolata,vertex)
-
+    Return: Ritorna un dizionario con tutte le distanze dal nodo di start
+    
     """
-    # crea una mappa delle distanze, considerando inzialmente tutte le distanze a infinito
-    distances_map = {vertex: float("inf") for vertex in graph}
-    # imposta la distanza del vertice di start a 0
-    distances_map[start] = 0
-    # mette in coda il primo elemento
-    queue = [(start, 0)]
 
-    while queue:
-        current_node, current_weight = min_pop(queue)
-        # per ogni vicino del nodo corrente
-        for neighbour in graph[current_node]:
-            # fa un calcolo della distanza dal nodo di start al nodo vicino
-            estimated_distance = current_weight + graph[neighbour][current_node]
-            # se la distanza stimata è minore della distanza nella distances map
-            if estimated_distance < distances_map[neighbour]:
-                # ha trovato una distanza minore, allora aggiorna il valore nella mappa delle distanze
-                distances_map[neighbour] = estimated_distance
-                queue.append((neighbour, estimated_distance))
-        # break
+    # crea un dizionario con le distanze tutte ad infinito
+    distances = {vertex: float("inf") for vertex in graph}
+    # considera la distanza dal nodo di start al nodo di start 0.
+    distances[start] = 0
 
-    return distances_map
+    # coda di priorità
+    pq = [(0, start)]
 
+    while pq:
+        # estrae dalla coda di priorità la tupla con il valore minimo di distanza
+        curr_w, curr_v = min_pop(pq)
+        
+        # per ogni vicino e il suo relativo peso
+        for neighbor, weight in graph[curr_v].items():
+            # fa una stima della distanza tra il nodo di start e uil nodo vicino
+            estimated_distance = weight + curr_w
+            # se questa stima è minore della distanza finora trovata
+            if estimated_distance < distances[neighbor]:
+                # aggiorna il valore di distanza minima
+                distances[neighbor] = estimated_distance
+                pq.append((estimated_distance, neighbor))
 
-def min_pop(queue):
-    """
-    Effettua un pop della tupla con il valore del peso minore
-
-    Args: La funzione prende in input una coda
-
-    Returns: Ritorna il nome e il peso minimo in coda
-
-    """
-    min_vertex, min_weight = queue[0]
-
-    for edge in queue:
-        curr_vertex, curr_w = edge
-        if curr_w < min_weight:
-            min_weight = curr_w
-            min_vertex = curr_vertex
-
-    queue.remove((min_vertex, min_weight))
-    return min_vertex, min_weight
+    return distances
 
 
 def main():
-    # grafo pesato non orientato
+    # grafo non orientato, pesato
     graph = {
         "ME": {"CT": 93.6, "PA": 225.8},
         "PA": {"CT": 209.6, "ME": 225.8, "TP": 108.5},
@@ -76,9 +76,9 @@ def main():
         "CL": {"SR": 159.6},
     }
 
-    print("Dijkstra result -->", dijkstra(graph, "ME"))
+    print("Dijkstra : ", dijkstra(graph, "ME"))
     print()
-    print("Graph -->", graph)
+    print("Graph:", graph)
 
 
 if __name__ == "__main__":
